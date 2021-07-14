4c4fe0f72f16bcb6e17964efcf5d7e8bc8b9bb38
wyl <2073027440@qq.com>
2021-07-14 13:51:46 +0800

    实现一个mobx小例子

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import stores from './mobx/index';
 import './Common/styles/index.scss';
 import App from './App';
+import BarBox from './Box/BarBox/index';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider {...stores}>
+    <BarBox />
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/mobx/index.tsx
@@ -0,0 +1,8 @@
+import {store} from './store';
+
+const stores:any = {
+  store
+};
+
+/// 默认导出接口
+export default stores;
