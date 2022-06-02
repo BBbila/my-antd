8dac78b6ad462a428f78b48782aa362b5bc30566
bbila <2073027440@qq.com>
2022-06-02 16:31:21 +0800

    draft

--- src/Box/MenuBox/index.tsx
+++ src/Box/MenuBox/index.tsx
@@ -1,12 +1,23 @@
 import { observer, inject } from "mobx-react";
 import { Fragment } from "react";
+import Menu from "../../Components/Menu/index";
+import MenuItem from "../../Components/Menu/compotents/MenuItem/index";
 
 interface Iprops {
   children?: any;
 }
 
 const MenuBox = ({ props }: Iprops & any) => {
-  return <Fragment>222</Fragment>;
+  return <Fragment>
+    <Menu index={0} onSelect={() => {}}>
+      <MenuItem>
+        第一
+      </MenuItem>
+      <MenuItem>
+        第二
+      </MenuItem>
+    </Menu>
+    </Fragment>;
 };
 
 export default inject((store) => store)(observer(MenuBox));
